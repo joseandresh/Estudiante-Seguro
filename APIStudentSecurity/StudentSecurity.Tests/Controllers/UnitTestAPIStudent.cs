0e695083161108d9f4c8ffe3abf5ce7e23a77c89
joseandresh <a2019112027@estudiantes.upsa.edu.bo>
2020-09-05 13:17:55 -0400

    Se creo el UnitTest del APIStudent.

---
+++ APIStudentSecurity/StudentSecurity.Tests/Controllers/UnitTestAPIStudent.cs
@@ -0,0 +1,23 @@
+﻿using APIStudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSecurity.Tests.Controllers
+{
+	[TestClass]
+	public class UnitTestAPIStudent
+	{
+		[TestMethod]
+		public void TestGetStudent()
+		{
+			//Arrange
+			StudentsController studentsController = new StudentsController();
+
+			//Act
+			var listaEstudiantes = studentsController.GetStudents();
+
+			//Assert
+			Assert.IsNotNull(listaEstudiantes);
+
+		}
+	}
+}
