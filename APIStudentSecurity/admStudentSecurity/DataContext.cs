f3ab7a56af2659271f0b53d07ac4eb784864a3c4
joseandresh <a2019112027@estudiantes.upsa.edu.bo>
2020-09-05 11:56:57 -0400

    Se agrego el proyecto MVC en adm.

---
+++ APIStudentSecurity/admStudentSecurity/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace admStudentSecurity
+{
+	using System.Data.Entity;
+	public class DataContext : DbContext
+	{
+		public DataContext() : base("DefaultConnection")
+		{
+
+		}
+		public System.Data.Entity.DbSet<admStudentSecurity.Student> Students { get; set; }
+	}
+}
---
+++ APIStudentSecurity/admStudentSecurity/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace admStudentSecurity
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	public class Student
+	{
+		[Key]
+		public int StudentId { get; set; }
+
+		[StringLength(30)]
+		[Required(ErrorMessage = "You must enter the field {0}")]
+		public string LastName { get; set; }
+
+		[StringLength(30, ErrorMessage = "The field {0} must between {2} and {1} characters", MinimumLength = 2)]
+		[Required(ErrorMessage = "You must enter the field {0}")]
+		public string FirstName { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:dd/MM/yyyyy}", ApplyFormatInEditMode = true)]
+		public DateTime EnrollmentDate { get; set; }
+	}
+}
